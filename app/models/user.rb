eea25c09bac08b189584bad5b34755d53c7d5eaa
Roman <romakorobko@gmail.com>
2018-04-15 19:20:14 +0300

    add paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,15 @@
 class User < ApplicationRecord
+  has_attached_file :avatar, styles: {
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+  }
+
   mount_uploader :avatar, AvatarUploader
   validates :name, presence: true
   validates :surname, presence: true
   validates :date, presence: true
 
+  validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
 
 end
