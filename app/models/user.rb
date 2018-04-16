c2127a512d0b73e18dab4e63da0388561838f65d
Roman <romakorobko@gmail.com>
2018-04-16 14:28:57 +0300

    replace validation paperclip

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,4 @@
 class User < ApplicationRecord
-  has_attached_file :avatar, styles: {
-      thumb: '100x100>',
-      square: '200x200#',
-      medium: '300x300>'
-  }
 
   mount_uploader :avatar, AvatarUploader
   validates :name, presence: true
@@ -11,5 +6,10 @@ class User < ApplicationRecord
   validates :date, presence: true
 
   validates_attachment_content_type :avatar, :content_type => /\Aimage\/.*\Z/
+  has_attached_file :avatar, styles: {
+      thumb: '100x100>',
+      square: '200x200#',
+      medium: '300x300>'
+  }
 
 end
